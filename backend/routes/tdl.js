88e1da03d9f9d9a5a42222fff2bbce3756d5412b
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-01 23:47:26 +0530

    created addtask API

--- backend/routes/tdl.js
+++ backend/routes/tdl.js
@@ -15,4 +15,22 @@ router.get("/display",async(req,res)=>{
     res.send(await TaskModel.find({}))
 })
 
+router.post("/addtask",async(req,res)=>{
+  try{
+    const { sno, task, date, time } = req.body;
+
+    const newTask = new TaskModel({
+        sno,
+        task,
+        date,
+        time
+    });
+    const savedTask = await newTask.save();
+
+    res.status(201).json(savedTask);
+  }catch(err){
+    res.status(400).json({ message: err.message });
+  }
+})
+
 module.exports = router
