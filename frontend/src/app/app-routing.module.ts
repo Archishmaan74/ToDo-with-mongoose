98a600f408d162640e6f8c67f7e99bcb62e4b4bd
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-01 23:19:03 +0530

    initialised routes for tasks & addtask component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { AddtaskComponent } from './addtask/addtask.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks', component: TasksComponent
+  },
+  {
+    path: 'addtask', component: AddtaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
