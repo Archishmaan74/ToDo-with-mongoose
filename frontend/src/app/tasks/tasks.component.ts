a9497a52cc0fa89f1ae8f3ab16cf8ba451021922
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-02 20:31:00 +0530

    created get logic for tasks component

--- frontend/src/app/tasks/tasks.component.ts
+++ frontend/src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TdlService } from '../tdl.service';
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-
+  tasks: any=[]
+  constructor(private tdlservice: TdlService){
+    this.tdlservice.getAlltasks().subscribe((data)=>{
+      this.tasks = data
+    },(err)=>{
+      console.log("Cannot get tasks...");
+    })
+  }
 }
