fa626789c4e6a93d51fab0011b5795a1c2d68f41
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-04 13:22:28 +0530

    subscribed params and task to display

--- frontend/src/app/edittask/edittask.component.ts
+++ frontend/src/app/edittask/edittask.component.ts
@@ -11,6 +11,16 @@ export class EdittaskComponent {
   task: any
   constructor(private aRoute: ActivatedRoute, private tdlService: TdlService){
     aRoute.params.subscribe((data)=>{
+      this.tdlService.getEditTask(data['SNO']).subscribe((data)=>{
+        this.task = data
+      },(err)=>{
+        console.log("Cannot fetch one task for editing...");
+      })
     })
+  }
+
+  editTask(task: any){
+    this.tdlService.editTask(task).subscribe(task)
+    alert("Task Edited successfully!")
   }
 }
